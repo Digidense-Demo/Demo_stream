d4b8c7370d4c37bbd27bdeba4329c4f3eebe3e09
Ashwini Kanagaraj <137770414+ASHWINISHEEBHA@users.noreply.github.com>
2024-04-04 14:55:16 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -7,5 +7,7 @@ terraform {
   }
 }
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIAVRUVWAXYDX3D4E5U"
+  secret_key = "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"
 }
