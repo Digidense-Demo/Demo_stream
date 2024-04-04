8a6aa8a6d5a8d111b9c17a06b7ea16942701c7fa
Ashwini Kanagaraj <137770414+ASHWINISHEEBHA@users.noreply.github.com>
2024-04-04 11:25:29 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "description" {
   type        = string
   description = "The description for the KMS key"
-  default = "Kms_key"
+  default     = "Kms_key"
 }
 
 variable "deletion_window_in_days" {
   type        = number
   description = "The deletion window in days"
-  default = 7
+  default     = 7
 }
 
 variable "enable_key_rotation" {
   type        = bool
   description = "Enable key rotation"
-  default = true
+  default     = true
 }
 
 variable "aliases_name" {
-  type = string
+  type        = string
   description = "this is for kms key name creation"
-  default = "alias/Demo_stream_key"
+  default     = "alias/Demo_stream_key"
 }
